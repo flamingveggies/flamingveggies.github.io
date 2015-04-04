f9340686d8a476eb5cda91ebfcac9e0c3e7771e6
Merritt Lawrenson <flamingveggies@users.noreply.github.com>
2015-04-03 21:20:53 -0700

    Initial design
    basic design + about page contents

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $('#load_about').click(function () {
+    $('#page').load('about.html');
+  });
+  $('#load_projects').click(function () {
+    $('#page').load('projects.html');
+  });
+  $('#load_contact').click(function () {
+    $('#page').load('contact.html');
+  });
+
+});
