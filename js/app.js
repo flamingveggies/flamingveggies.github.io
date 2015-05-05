d8ea5c7faa1eed2e7230297c7b9faa0a8fe07156
Merritt Lawrenson <flamingveggies@users.noreply.github.com>
2015-05-05 03:11:34 -0700

    added photography page and favicon
    also updated file structure

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$('#load_about').click(function () {
+  $('#page').load('about.html');
+});
+$('#load_projects').click(function () {
+  $('#page').load('projects.html');
+});
+$('#load_photography').click(function () {
+  $('#page').load('photography.html');
+});
+$('#load_contact').click(function () {
+  $('#page').load('contact.html');
+});
